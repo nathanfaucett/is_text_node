f81ed698b2a1154591504c47ae998353bbbebbbe
Nathan Faucett <nathanfaucett@gmail.com>
2015-04-30 12:57:53 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 var isNode = require("is_node");
 
 
-module.exports = function isTextNode(obj) {
+module.exports = isTextNode;
+
+
+function isTextNode(obj) {
     return isNode(obj) && obj.nodeType === 3;
-};
+}
