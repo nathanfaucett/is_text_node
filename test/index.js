c15e9164ae613e97bccf72bc388baeffa76c67d4
nathanfaucett <nathanfaucett@gmail.com>
2015-10-18 10:23:04 -0500

    updates

--- test/index.js
+++ test/index.js
@@ -1,11 +1,20 @@
 var tape = require("tape"),
+    isFunction = require("is_function"),
     isTextNode = require("..");
 
 
 tape("isTextNode(value) should return true when the value is a TextNode", function(assert) {
-    assert.equal(isTextNode({
-        nodeName: "div",
-        nodeType: 3
-    }), true);
+    var node;
+
+    if (typeof(Node) !== "undefined" && isFunction(Node)) {
+        node = document.createTextNode("");
+        assert.equal(isTextNode(node), true);
+    } else {
+        assert.equal(isTextNode({
+            nodeName: "#text",
+            nodeType: 3
+        }), true);
+    }
+
     assert.end();
 });
